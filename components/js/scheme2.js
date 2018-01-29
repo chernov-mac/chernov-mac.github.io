aa57bb301bfc74d2721d186c395f8a7c945869dd
Vladimir Chernov <inky@host-192-168-1-48.soho.net.ua>
2018-01-29 19:15:49 +0200

    New way of pinch scaling math [2]

--- components/js/scheme2.js
+++ components/js/scheme2.js
@@ -1,6 +1,6 @@
 (function($) {
 
-    var version = '0.1.14';
+    var version = '0.1.15';
     var enableScaleControls = false;
     var logging = true;
     var pinchLogged = true;
@@ -454,7 +454,7 @@
         //     delta = -delta;
         // }
 
-        var delta = Math.abs(Math.round(delta * 100) / 100);
+        var delta = Math.abs(Math.round(ev.scale * 100) / 100);
         var newScale = scale * delta;
 
         // var newScale = getScaleWithDelta(delta);
